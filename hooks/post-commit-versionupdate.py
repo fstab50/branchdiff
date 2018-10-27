cd3b42430b2066a18863ff16bfe7c45d62bbd7f0
Blake Huber <blakeca00@gmail.com>
2018-10-27 13:29:49 -0500

    hooks/post-commit-versionupdate.py: major updates

--- hooks/post-commit-versionupdate.py
+++ hooks/post-commit-versionupdate.py
@@ -9,41 +9,50 @@
 import os
 import sys
 import inspect
+import fileinput
+from pyaws.utils import stdout_message
+
+version_module = 'version.py'
 
 
 def packagename(filename):
-    with open(filename) as p1:
-        for line in p1.readlines():
-            if 'PACKAGE' in line:
-                return line.split(':')[1].strip()
-                break
+    """Retrieve name of build package stored locally."""
+    if os.path.exists(filename):
+        with open(filename) as p1:
+            for line in p1.readlines():
+                if 'PACKAGE' in line:
+                    return line.split(':')[1].strip()
+                    break
+                else:
+                    return None
+    return None
 
 
-PACKAGE = packagename('DESCRIPTION.rst') or None
+PACKAGE = packagename('DESCRIPTION.rst')
 
 if PACKAGE is None:
-    print('Problem executing post-commit-hook (%s). Exit' % __file__)
+    stdout_message(
+            message='Problem executing post-commit-hook (%s). Exit' % __file__,
+            prefix='WARN'
+        )
     sys.exit(1)
 else:
     sys.path.insert(0, os.path.abspath(PACKAGE))
-    from _version import __version__
+    from version_module import __version__
     sys.path.pop(0)
 
 
 try:
-    with open('README.md') as f1:
-        lines = f1.readlines()
-        for index, line in enumerate(lines):
-            if 'Version:' in line:
-                newline = ' '.join(line.split(' ')[:2]) + ' Version: ' + __version__ + '\n'
-                lines[index] = newline
-                break
-        f1.close()
-        with open('README.md', 'w') as f3:
-            f3.writelines(lines)
+
+    if os.path.exists('README.md'):
+        # update specfile - major version
+        for line in fileinput.input(['README.md'], inplace=True):
+            print(line.replace('MAJOR_VERSION', major), end='')
+        stdout_message(f'Updated {specfile} with MAJOR_VERSION', prefix='OK')
+
 except OSError as e:
-    print(
-            '%s: Error while reading or writing post-commit-hook' %
-            inspect.stack()[0][3]
+    stdout_message(
+            message='%s: Error while reading or writing post-commit-hook' % inspect.stack()[0][3],
+            prefix='WARN'
         )
 sys.exit(0)
