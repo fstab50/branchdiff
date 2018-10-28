67e8b069817a2e4fe5ef6e19e9182bf8fc6985bc
Blake Huber <blakeca00@gmail.com>
2018-10-28 09:34:36 -0500

    bash, core: updates

--- bash/branchdiff-completion.bash
+++ bash/branchdiff-completion.bash
@@ -23,7 +23,7 @@
 # SOFTWARE.
 
 
-function _current_downloads(){
+function _branch_subcommands(){
     ##
     ##  Examines local fs for downloaded artifacts
     ##
@@ -58,19 +58,29 @@ function _current_downloads(){
 
 function _complete_branchdiff_commands()
 {
-  local cmds="$1"
-  local split='4'       # times to split screen width
-  local IFS=$' \t\n'
-  local formatted_cmds=( $(compgen -W "${cmds}" -- "${COMP_WORDS[1]}") )
+    local cmds="$1"
+    local split='2'       # times to split screen width
+    local ct="0"
+    local IFS=$' \t\n'
+    local formatted_cmds=( $(compgen -W "${cmds}" -- "${COMP_WORDS[1]}") )
 
-  for i in "${!formatted_cmds[@]}"; do
-      formatted_cmds[$i]="$(printf '%*s' "-$(($COLUMNS/$split))"  "${formatted_cmds[$i]}")"
-  done
+    for i in "${!formatted_cmds[@]}"; do
 
-  COMPREPLY=( "${formatted_cmds[@]}")
-  return 0
-  #
-  # <-- end function _complete_branchdiff_commands -->
+        if [ $(( $ct % 2 )) -eq "0" ]; then
+            #formatted_cmds[$i]="$(echo ${formatted_cmds[$i]})"
+            formatted_cmds[$i]="$(printf '%*s' "-$((2))"  "${formatted_cmds[$i]}")"
+        else
+            formatted_cmds[$i]="$(printf '%*s' "-$(($COLUMNS))"  "${formatted_cmds[$i]}")"
+        fi
+
+        (( ct++ ))
+
+    done
+
+    COMPREPLY=( "${formatted_cmds[@]}")
+    return 0
+    #
+    # <-- end function _complete_branchdiff_commands -->
 }
 
 
