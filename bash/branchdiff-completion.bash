caf1c5c3621d89775c2edf1c0500b8428a862067
Blake Huber <blakeca00@gmail.com>
2018-11-20 10:58:46 -0600

    bash/branchdiff-completion.bash: commit-log sub cmd updates final

--- bash/branchdiff-completion.bash
+++ bash/branchdiff-completion.bash
@@ -125,7 +125,7 @@ function _branchdiff_completions(){
 
     # option strings
     commands='--branch --code --commit-log --debug --help --version'
-    commitlog_subcommands='commit-details history history-detail summary'
+    commitlog_subcommands='merge-detail history history-detail summary'
     operations='--branch --code'
 
     # subcommand sets
@@ -185,7 +185,7 @@ function _branchdiff_completions(){
             return 0
             ;;
 
-        'commit-details' | 'history' | 'history-detail' | 'summary')
+        'merge-detail' | 'history' | 'history-detail' | 'summary')
             # --commit-log subcommands completed; stop
             return 0
             ;;
